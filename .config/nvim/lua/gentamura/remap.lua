96c9e84851b25aadd1863d2a627272c6329dfe8e
Gen Tamura <gen.tamura84@gmail.com>
2023-01-28 18:22:02 +0900

    Add comments in keymap files

--- .config/nvim/lua/gentamura/remap.lua
+++ .config/nvim/lua/gentamura/remap.lua
@@ -1,2 +1,11 @@
+-- Modes
+--   normal_mode = 'n',
+--   insert_mode = 'i',
+--   visual_mode = 'v',
+--   visual_block_mode = 'x',
+--   term_mode = 't',
+--   command_mode = 'c',
+
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("c", "cwd", "echo expand('%')<CR>")
