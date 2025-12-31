51b049278bacbd2b89a080034ec27bcdded0fad9
Gen Tamura <gen.tamura84@gmail.com>
2025-12-31 22:57:41 +0900

    Merge home dotfiles settings

--- .zshrc.d/30-aliases.zsh
+++ .zshrc.d/30-aliases.zsh
@@ -3,3 +3,13 @@ alias ls='eza'
 alias ll='ls -la'
 alias cat='bat'
 alias pn='pnpm'
+
+# Git worktree
+alias gwtl='git worktree list'
+alias gwtadd='git worktree add'
+alias gwtaddb='git worktree add -b'
+alias gwtrm='git worktree remove'
+alias gwtprune='git worktree prune'
+alias gwtmv='git worktree move'
+alias gwtshow="awk '/^# Git worktree/{flag=1;next}/^# ---/{if(flag)exit}flag' ~/.zshrc.d/30-aliases.zsh"
+# ---
