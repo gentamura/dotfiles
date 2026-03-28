74aae1fcd405e4ead724d468948fbb9810611b61
Gen Tamura <gen.tamura84@gmail.com>
2026-03-28 17:21:36 +0900

    fix(zsh): avoid tmux session name collisions in tms

--- .zshrc.d/40-functions.zsh
+++ .zshrc.d/40-functions.zsh
@@ -9,7 +9,44 @@ zle -N peco-history-selection
 bindkey '^R' peco-history-selection
 
 function tms() {
-  local name="${PWD##*/}"
+  local cwd="${PWD:A}"
+  local name="${cwd##*/}"
+  local candidate=""
+  local full_candidate=""
+  local start=1
+  local count=0
+  local i=0
+  local suffix=2
+  local -a parts
+
   name="${name// /_}"
-  tmux new -s "$name"
+  if ! tmux has-session -t "$name" 2>/dev/null; then
+    tmux new -s "$name"
+    return
+  fi
+
+  parts=("${(@s:/:)cwd}")
+  parts=("${parts[@]:#}")
+  count=${#parts[@]}
+
+  for ((i = 2; i <= count; i++)); do
+    start=$((count - i + 1))
+    candidate="${(j:/:)parts[$start,$count]}"
+    candidate="${candidate//\//__}"
+    candidate="${candidate// /_}"
+    if ! tmux has-session -t "$candidate" 2>/dev/null; then
+      tmux new -s "$candidate"
+      return
+    fi
+  done
+
+  full_candidate="${(j:__:)parts}"
+  full_candidate="${full_candidate// /_}"
+  candidate="${full_candidate}-${suffix}"
+  while tmux has-session -t "$candidate" 2>/dev/null; do
+    suffix=$((suffix + 1))
+    candidate="${full_candidate}-${suffix}"
+  done
+
+  tmux new -s "$candidate"
 }
