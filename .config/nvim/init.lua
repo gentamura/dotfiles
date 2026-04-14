a2b78dcd1210e9a096fd80df64e389f9d5a980ef
Gen Tamura <gen.tamura84@gmail.com>
2026-04-14 14:07:06 +0900

    Update lualine path

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -120,7 +120,7 @@ require("lazy").setup({
             {
               "filename",
               file_status = true, -- displays file status (readonly status, modified status)
-              path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
+              path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
             },
           },
           lualine_x = {
